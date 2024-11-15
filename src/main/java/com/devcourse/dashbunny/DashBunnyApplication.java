8fb1f589b27c88364dd7b3efe6816ffc814225a5
Web도승우 <140596632+MagongDo@users.noreply.github.com>
2024-11-15 14:04:34 +0900

    Add files via upload

---
+++ src/main/java/com/devcourse/dashbunny/DashBunnyApplication.java
@@ -0,0 +1,13 @@
+package com.devcourse.dashbunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashBunnyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashBunnyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devcourse/dashbunny/DashBunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcourse.dashbunny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashBunnyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
